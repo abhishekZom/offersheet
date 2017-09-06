45f7e1b9541dcd69d449c90eec931b219fc9aaf1
Abhishek Kumar <abkumar1@q3tech.net>
2017-09-06 11:16:14 +0530

    latest stable

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ offersheet.config(['$routeProvider', function($routeProvider){
         templateUrl:'./templates/offersheet.html',
         controller: 'appController as app',
         resolve: {
-            products: function(appService) {
+            data: function(appService) {
                 return appService.getAllData(0, 100);
             }
         }
@@ -25,23 +25,44 @@ offersheet.run(['$rootScope', function($rootScope) {
         $('#mainLoader').hide();
     }
     
+    // on route change success
     $rootScope.$on('$routeChangeSuccess', function(event, toState, toParams, fromState, fromParams) {
         $rootScope.hideSpinner();
+    });
+
+    // on route change error
+    $rootScope.$on('$routeChangeError', function(event, toState, toParams, fromState, fromParams, error){ 
+            // this is required if you want to prevent the $UrlRouter reverting the URL to the previous valid location
+            event.preventDefault();
+            $rootScope.hideSpinner();
+            alertService.alertFailure('failed to load data!');
+
     });
 
     $rootScope.productsPage = 1;
     $rootScope.productsCount = 100;
 }]);
 
-offersheet.controller('imageModalController', ['$scope', '$uibModalInstance', function($scope, $uibModalInstance) {
-    $scope.imageUrl = "https://akamaicdn3.shoplc.com/LCProdImage/LCProdImage/";
+
+
+
+// contoller for image modal popup
+offersheet.controller('imageModalController', ['$scope', '$uibModalInstance', 'value', function($scope, $uibModalInstance, value) {
+    $scope.value = value;
+    $scope.imageUrl = "https://akamaicdn3.shoplc.com/LCProdImage/LCProdImage/" + $scope.value.image;
+    $scope.close = function() {
+        $uibModalInstance.close();
+    }
 }]);
 
+
+
+
 // main app controller
-offersheet.controller('appController', ['$scope', '$rootScope', 'products', 'appService', 'alertService', '$timeout', 'utils', '$uibModal',function($scope, $rootScope, products, appService, alertService, $timeout, utils, $uibModal) {
+offersheet.controller('appController', ['$scope', '$rootScope', 'data', 'appService', 'alertService', '$timeout', 'utils', '$uibModal',function($scope, $rootScope, data, appService, alertService, $timeout, utils, $uibModal) {
     var timeout; // timeout initialization for event debouncing
     $scope.shadowProducts = {};
-    $scope.shadowProducts = angular.copy(products);
+    $scope.shadowProducts = angular.copy(data.data);// this is for the purpose of flashbacks and model comparisons
 
     // initialize controller setup
     init();
@@ -52,34 +73,64 @@ offersheet.controller('appController', ['$scope', '$rootScope', 'products', 'app
      */
     function init() {
         // scope var bindings
-        $scope.products = products;
+        $scope.products = data.data;
         $scope.isLoading = false;
-        $scope.productHeaders = $scope.products['header'];
-        $scope.recordCount = products.total;
+        $scope.productHeaders = data.info.headers;
+        $scope.recordCount = data.info.total;
         $scope.isUpdating = false;
+
+        // scope method binding
+        $scope.updateData = updateData;
         $scope.loadNextShift = loadNextShift;
+        $scope.loadAllInSecondShift = loadAllInSecondShift;
         $scope.openImageModal = openImageModal;
 
-        // scope methods binding
-        $scope.updateData = updateData;
+
         if($scope.products) {
             //clean the products data and show success
-            delete $scope.products['header'];
-            delete $scope.products['total'];
             alertService.alertSuccess('data loaded successfully!');
-
-            // $scope.loadNextShift();
+            $scope.loadAllInSecondShift();
         } else {
             // log error and show alert
             alertService.alertFailure('failed to load data!');
         }
     }
 
-    function openImageModal(imageUrl) {
+    // method to open the image modal popup
+    function openImageModal(value) {
         $uibModal.open({
             templateUrl: './templates/imageModal.html',
-            controller: 'imageModalController'
+            controller: 'imageModalController',
+            resolve: {
+                value : function() {
+                    return value;
+                },
+
+            }
         });
+        $timeout(function() {
+            $('.modal').removeClass('fade');
+        }, 100);
+    }
+
+    function loadAllInSecondShift() {
+        var allPromiseArray = [];
+        for(var i=0; i<=10; i++) {
+            allPromiseArray.push(appService.getAllData($rootScope.productsPage, $rootScope.productsCount));
+            $rootScope.productsPage++;
+        }
+
+        Promise.all(allPromiseArray).then(function(data) {
+            data.forEach(function(ele) {
+                 $scope.shadowProducts = $scope.shadowProducts.concat(ele.data);
+            });
+            $timeout(function() {
+                $scope.products = $scope.shadowProducts;
+            }, 0);
+        }).catch(function(err) {
+            console.log(err);
+            throw err;
+        })
     }
 
     /**
@@ -93,18 +144,16 @@ offersheet.controller('appController', ['$scope', '$rootScope', 'products', 'app
         $timeout(function() {
             appService.getAllData($rootScope.productsPage, $rootScope.productsCount)
             .then(function(res) {
-                $scope.recordCount += res.total;
-                delete res['header'];
-                delete res['total'];
+                $scope.recordCount += res.info.total;
                 if(res) {
                     // write check for the length of the returned response
-                    if((utils.getObjlength(res) > 0)) {
-                        angular.extend($scope.products, res);
+                    if(res.data.length > 0) {
+                        $scope.products = $scope.products.concat(res.data);
                         $scope.shadowProducts = angular.copy($scope.products);
                         $rootScope.productsPage++;
                         $scope.loadNextShift();
                     }
-                    else if(!(utils.getObjlength(res) > 0)) {
+                    else if(!((res.data.length) > 0)) {
                         alertService.alertSuccess('all data loaded successfully!');
                         return;
                     }
@@ -124,14 +173,14 @@ offersheet.controller('appController', ['$scope', '$rootScope', 'products', 'app
      * @param {String} prop     prop name for the colume to be updated
      * @param {Number} index    index of current row
      */
-    function updateData(key,value, prop, index) {
-        //clear timeout as soon as user types again within a second
+    function updateData(nid, value, prop, index) {
+        //clear timeout aearTimeout(times soon as user types again within a second
         clearTimeout(timeout);
         timeout = setTimeout(function() {
             //code in this block will execute once user has not typed anything for one second
 
             // check if the value entered has actually changed the initial value
-            if($scope.products[key][prop] == $scope.shadowProducts[key][prop]) {
+            if($scope.products[index][prop] == $scope.shadowProducts[index][prop]) {
                 alertService.alertFailure('field value is unchanged!');
                 return;
             }
@@ -142,7 +191,7 @@ offersheet.controller('appController', ['$scope', '$rootScope', 'products', 'app
                 if(str.charCodeAt(i) <48 || str.charCodeAt(i) >57) {
                     // show alert and return input is invalid
                     $scope.$apply(function() {
-                        $scope.products[key][prop] = $scope.shadowProducts[key][prop];
+                        $scope.products[index][prop] = $scope.shadowProducts[index][prop];
                     });
                     alertService.alertFailure('failed to update data! Only numbers Allowed!');
                     return;
@@ -153,9 +202,8 @@ offersheet.controller('appController', ['$scope', '$rootScope', 'products', 'app
             $('#loader_'+ prop + index).show();
             $('#input_'+ prop + index).hide();
             $scope.isUpdating = true;
-
             // hit the api for updation
-            appService.updateData(key,value)
+            appService.updateData(nid,value)
             .then(function(res) {
                 if(res.data && res.message == "success") {
                     // on success bind data, update ui to hide loader and enable all inputs
@@ -164,8 +212,8 @@ offersheet.controller('appController', ['$scope', '$rootScope', 'products', 'app
                         $scope.isUpdating = false;
                     });
                     alertService.alertSuccess('data updated successfully!');
-                    $scope.products[key][prop] = res.data[prop];
-                    $scope.shadowProducts[key][prop] = $scope.products[key][prop];
+                    $scope.products[index][prop] = res.data[prop];
+                    $scope.shadowProducts[index][prop] = $scope.products[index][prop];
                     $('#loader_'+ prop + index).hide();
                     $('#input_'+ prop + index).show();
                     // -------------------------------------
@@ -175,7 +223,7 @@ offersheet.controller('appController', ['$scope', '$rootScope', 'products', 'app
                 alertService.alertFailure('failed to update data!');
                 console.log(err);
             });
-        }, 500);
+        }, 1000);
     }
 }]);
 
@@ -195,11 +243,11 @@ offersheet.service('appService', ['$http', function($http) {
     };
 
     // update data from table grid
-    this.updateData = function(key, value) {
+    this.updateData = function(nid, value) {
         var payload = JSON.stringify(value);
         return new Promise(function(resolve, reject) {
             jQuery.ajax({
-                url: "http://115.113.189.18/vglorder/vpop_dev/offersheet/api/1.0/vgl_offer_sheet/" + key,
+                url: "http://115.113.189.18/vglorder/vpop_dev/offersheet/api/1.0/vgl_offer_sheet/" + nid,
                 type: 'PUT',
                 contentType: "application/json",
                 data: payload,
@@ -282,3 +330,21 @@ offersheet.service('utils', function() {
          }; 
     };
 });
+
+
+// custom filter for implementing default angular filters for ng-repeat over object
+offersheet.filter('toArray', function () {
+    return function (obj, addKey) {
+      if (!(obj instanceof Object)) {
+        return obj;
+      }
+  
+      if ( addKey === false ) {
+        return Object.values(obj);
+      } else {
+        return Object.keys(obj).map(function (key) {
+          return Object.defineProperty(obj[key], '$key', { enumerable: false, value: key});
+        });
+      }
+    };
+  });
